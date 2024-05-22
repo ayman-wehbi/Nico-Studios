734a630ba4273dd4da88ee187bd72609ef354525
Ayman Wehbi <145095885+ayman-wehbi@users.noreply.github.com>
2024-05-22 15:10:26 -0500

    Add files via upload

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Home from './src/screens/HomeScreen';
 import Settings from './src/screens/SettingsScreen';
 import Projects from './src/screens/ProjectsScreen';
@@ -8,14 +8,18 @@ import SongScreen from './src/screens/SongScreen';
 import SongList from './src/screens/SongList';
 import LoginScreen from './src/screens/LoginScreen.js';
 import CreateScreen from './src/screens/CreateScreen';
+import IndexScreen from './src/screens/IndexScreen.js';
 import CreateSongScreen from './src/screens/CreateSongScreen';
-import EditScreen from './src/screens/EditScreen';
+import AccountScreen from './src/screens/AccountScreen.js'
 import TaskScreen from './src/screens/TaskScreen';
+import PwResetScreen from './src/screens/PwResetScreen.js';
 import ProjectsScreen from './src/screens/ProjectsScreen';  
 import ProjectDetails from './src/screens/ProjectDetails';
+import SignupScreen from './src/screens/SignupScreen.js';
 import { StatusBar } from 'react-native';
 import Toast from 'react-native-toast-message';
 import { Provider } from './src/context/NoteContext';
+import { auth, firestore, firebase } from './firebase';
 import { CardStyleInterpolators } from '@react-navigation/stack';
 
 const Stack = createStackNavigator();
@@ -27,13 +31,35 @@ const fadeInOut = ({ current }) => ({
 });
 
 const App = () => {
+  const [loading, setLoading] = useState(true);
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(user => {
+      if (user) {
+        firestore.collection('users').doc(user.uid).get()
+          .then((document) => {
+            const userData = document.data();
+            setLoading(false);
+            setUser(userData);
+          })
+          .catch((error) => {
+            setLoading(false);
+          });
+      } else {
+        setLoading(false);
+      }
+    });
+    // Cleanup subscription on unmount
+    return () => unsubscribe();
+  }, []);
   return (
     <Provider>
       <NavigationContainer>
         <StatusBar backgroundColor="#face88" barStyle="dark-content" />
         <Toast ref={(ref) => Toast.setRef(ref)} />
         <Stack.Navigator
-          initialRouteName="LoginScreen"
+          initialRouteName={"IndexScreen"}
           screenOptions={{
             headerTitle: 'Nico Studios',
             cardStyleInterpolator: ({ current, layouts }) => {
@@ -66,11 +92,15 @@ const App = () => {
           <Stack.Screen name="SongList" component={SongList} options={{ headerTitle: 'Songs', headerLeft: () => null, cardStyleInterpolator: fadeInOut }} />
           <Stack.Screen name="TaskScreen" component={TaskScreen} />
           <Stack.Screen name="SongScreen" component={SongScreen} options={{ headerTitle: 'Song'}}/>
-          <Stack.Screen name="Settings" component={Settings} />
+          <Stack.Screen name="Settings" component={Settings} options={{ headerTitle: 'Settings' }}/>
           <Stack.Screen name="CreateSongScreen" component={CreateSongScreen} />
           <Stack.Screen name="CreateScreen" component={CreateScreen} />
+          <Stack.Screen name="SignupScreen" component={SignupScreen} options={{ headerTitle: 'Sign Up' }} />
           <Stack.Screen name="ProjectsScreen" component={ProjectsScreen} />
+          <Stack.Screen name="IndexScreen" component={IndexScreen} options={{ headerShown: false }} />
           <Stack.Screen name="LoginScreen" component={LoginScreen}options={{ headerShown: false }} />
+          <Stack.Screen name="AccountScreen" component={AccountScreen}options={{headerTitle: 'Account'}} />
+          <Stack.Screen name="PwResetScreen" component={PwResetScreen}options={{ headerShown: false }} />
           <Stack.Screen name="ProjectDetails" component={ProjectDetails} options={{ headerTitle: 'Project Details' }} />
         </Stack.Navigator>
       </NavigationContainer>
